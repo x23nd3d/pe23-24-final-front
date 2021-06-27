7d27297d59d2a789fcdd5c6f0070a4b1588802ee
vitaliiverdiiev <vitaliiverdiiev@gmail.com>
2021-06-28 01:41:26 +0300

    Moved Slider from Layout to Main Page

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Header from "../../components/Main Page/Header/Header";
-import Slider from "../../components/Main Page/Slider/Slider";
 import classes from "./Layout.module.scss";
 
 const Layout = ({ children }) => (
   <div className={classes.Layout}>
     <Header />
-    <Slider />
+
     {children}
   </div>
 );
