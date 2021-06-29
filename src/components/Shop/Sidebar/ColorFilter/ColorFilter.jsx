e1798cd5f97ffb45a896064116b565127c015901
x23nd3d <x23nd3d@gmail.com>
2021-06-29 08:27:27 -0400

    added directories

---
+++ src/components/Shop/Sidebar/ColorFilter/ColorFilter.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import classes from "./ColorFilter.module.scss";
+
+const ColorFilter = (props) => <div className={classes.ColorFilter}></div>;
+
+export default ColorFilter;
---
+++ src/components/Shop/Sidebar/PriceFilter/PriceFilter.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import classes from "./PriceFilter.module.scss";
+
+const PriceFilter = (props) => <div className={classes.PriceFilter}></div>;
+
+export default PriceFilter;
