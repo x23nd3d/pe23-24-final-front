ca6c0080a1ef832f0526a50af8d21dee221cac29
vitaliiverdiiev <vitaliiverdiiev@gmail.com>
2021-06-29 17:21:53 +0300

    Added Color Filter. Static

--- src/components/Shop/Sidebar/ColorFilter/ColorFilter.jsx
+++ src/components/Shop/Sidebar/ColorFilter/ColorFilter.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import classNames from "classnames";
 import classes from "./ColorFilter.module.scss";
+import "./_colors.scss";
 
-const ColorFilter = (props) => <div className={classes.ColorFilter}></div>;
+const colors = ["black", "brown", "red", "white"];
+
+const ColorFilter = (props) => (
+  <div className={classes.ColorFilter}>
+    <h3 className={classes.Title}>Colors</h3>
+    <ul className={classes.Colors}>
+      {colors.map((color) => (
+        <li key={color} className={classNames(classes.Color, color)}>
+          {color}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 export default ColorFilter;
--- src/components/Shop/Sidebar/Sidebar.jsx
+++ src/components/Shop/Sidebar/Sidebar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ColorFilter from "./ColorFilter/ColorFilter";
 import classes from "./Sidebar.module.scss";
 
 const Sidebar = (props) => (
   <div className={classes.Sidebar}>
     <h1 style={{ color: "white" }}>Hello from Sidebar! :D</h1>
+    <ColorFilter />
   </div>
 );
 
