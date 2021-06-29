c11eb53535382dd988f097bb9b96c627776ec01e
vitaliiverdiiev <vitaliiverdiiev@gmail.com>
2021-06-29 23:35:42 +0300

    Added Price filter

--- src/components/Shop/Sidebar/PriceFilter/PriceFilter.jsx
+++ src/components/Shop/Sidebar/PriceFilter/PriceFilter.jsx
@@ -1,42 +1,27 @@
 import React, { useState } from "react";
 import InputRange from "react-input-range";
-import "react-input-range/lib/css/index.css";
+import "./_price-filter.scss";
 import classes from "./PriceFilter.module.scss";
 
 const PriceFilter = (props) => {
   const [rangeValue, setRangeValue] = useState({
-    min: 2,
-    max: 10,
+    min: 0,
+    max: 1500,
   });
 
   return (
     <div className={classes.PriceFilter}>
       <h3 className={classes.Title}>Price</h3>
-      <p className={classes.PriceRange}>$ 25 - 930</p>
+      <p className={classes.PriceRange}>
+        ${rangeValue.min} - ${rangeValue.max}{" "}
+      </p>
       <div className={classes.RangeBlock}>
         <InputRange
-          maxValue={20}
+          maxValue={1500}
           minValue={0}
           value={rangeValue}
           onChange={(value) => setRangeValue(value)}
         />
-        );
-        {/* <input
-          type="range"
-          min="1"
-          max="100"
-          value="0"
-          className="slider"
-          id="myRange"
-        />
-        <input
-          type="range"
-          min="1"
-          max="100"
-          value="100"
-          className="slider"
-          id="myRange"
-        /> */}
       </div>
     </div>
   );
--- src/components/Shop/Sidebar/Sidebar.jsx
+++ src/components/Shop/Sidebar/Sidebar.jsx
@@ -5,7 +5,6 @@ import classes from "./Sidebar.module.scss";
 
 const Sidebar = (props) => (
   <div className={classes.Sidebar}>
-    <h1 style={{ color: "white" }}>Hello from Sidebar! :D</h1>
     <ColorFilter />
     <PriceFilter />
   </div>
