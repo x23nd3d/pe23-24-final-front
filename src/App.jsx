9a87c14192a3781f56973fa306963bc00d141c87
x23nd3d <x23nd3d@gmail.com>
2021-06-29 09:04:47 -0400

    Added shop route

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.module.scss";
 import { Route, Switch } from "react-router-dom";
 import MainPage from "./components/Main Page/MainPage";
+import Shop from "./components/Shop/Shop";
 
 function App() {
   const routes = (
     <Switch>
       <Route path="/" component={MainPage} />
+      <Route path="/shop" component={Shop} />
     </Switch>
   );
 
