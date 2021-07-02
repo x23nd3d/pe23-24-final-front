b49995e070aaa17238245a4ef33410cf5a712c45
x23nd3d <x23nd3d@gmail.com>
2021-07-02 04:55:09 -0400

    Dynamic Dropdown Render + logic on click + small fixes

--- src/components/UI/NavigationListRoutes/NavigationListRoutes.jsx
+++ src/components/UI/NavigationListRoutes/NavigationListRoutes.jsx
@@ -1,25 +1,37 @@
 import React from "react";
-import PropTypes from "prop-types";
+import PropTypes, { instanceOf } from "prop-types";
 import classNames from "classnames";
 import { NavLink } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Dropdown from "../../Header/Navbar/Dropdown/Dropdown";
 
 const DropdownRoute = ({
   content,
   route,
   active,
+  id,
   activeClass,
   listClass,
   linkClass,
   dropdownToggle,
+  dropdownMenu,
 }) => {
   const activeCls = [active ? activeClass : null];
+
+  const toggleDropdown = (idx) => {
+    console.log(idx);
+    dropdownToggle((prev) => !prev);
+  };
   return (
     <li className={classNames(listClass)}>
+      <AnimatePresence>
+        {active && <Dropdown dropdownList={dropdownMenu.clothes} />}
+      </AnimatePresence>
       <NavLink
         to={route}
         activeClassName={activeCls.join(" ")}
         className={linkClass}
-        onClick={() => dropdownToggle()}
+        onClick={() => toggleDropdown(id)}
       >
         {content}
       </NavLink>
@@ -31,7 +43,10 @@ DropdownRoute.defaultProps = {
   activeClass: "",
   listClass: "",
   linkClass: "",
+  id: instanceOf(Number),
   active: false,
+  clothes: [],
+  dropdownMenu: {},
   dropdownToggle: () => console.log("Dropdown toggler"),
 };
 
@@ -42,7 +57,10 @@ DropdownRoute.propTypes = {
   listClass: PropTypes.string,
   linkClass: PropTypes.string,
   active: PropTypes.bool,
+  id: PropTypes.number,
   dropdownToggle: PropTypes.func,
+  clothes: PropTypes.instanceOf(Array),
+  dropdownMenu: PropTypes.instanceOf(Object),
 };
 
 export default DropdownRoute;
