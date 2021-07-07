a2903539f90b0f426fc4c5120d00d7f09b20c9ab
Vitalii Verdiiev <vitaliiverdiiev@gmail.com>
2021-07-07 13:12:10 +0300

    Added state for Color Filter

--- src/components/Shop/Sidebar/ColorFilter/ColorFilter.jsx
+++ src/components/Shop/Sidebar/ColorFilter/ColorFilter.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 import classes from "./ColorFilter.module.scss";
 
 const colors = ["black", "brown", "red", "white"];
 
-const ColorFilter = (props) => (
-  <div className={classes.ColorFilter}>
-    <h3 className={classes.Title}>Colors</h3>
-    <ul className={classes.Colors}>
-      {colors.map((color) => (
-        <li key={color} className={classNames(classes.Color, classes[color])}>
-          {color}
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const ColorFilter = (props) => {
+  const [chosenColor, setChosenColors] = useState({ color: "" });
+
+  const pickAColor = (color) => {
+    setChosenColors({
+      color: `${color.toLowerCase()}`,
+    });
+  };
+
+  return (
+    <div className={classes.ColorFilter}>
+      <h3 className={classes.Title}>Colors</h3>
+      <ul className={classes.Colors}>
+        {colors.map((color) => (
+          <li>
+            <a
+              href="#"
+              key={color}
+              onClick={() => pickAColor(color)}
+              className={classNames(classes.Color, classes[color])}
+            >
+              {color}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ColorFilter;
