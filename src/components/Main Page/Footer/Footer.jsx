7e07e6d2c1c064221673fafebc69cf6b4602f5d0
MacBook <macbook@MacBook-Pro-MacBook-3.local>
2021-07-07 14:32:35 +0300

    adaptive_footer

--- src/components/Main Page/Footer/Footer.jsx
+++ src/components/Main Page/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./Footer.module.scss";
 import ListRoute from "../../UI/ListRoute/ListRoute";
+import {ReactComponent as ArrowDown} from "./Footer img/arrow-down.svg";
 
 const Footer = () => {
   const footerListItems = {
@@ -45,19 +46,29 @@ const Footer = () => {
   return (
     <div className={classes.Footer}>
       <div className={classes.FooterColumn}>
-        <h3>Help</h3>
+        <div className={classes.ArrowDownColumnContainer}>
+          <h3>Help</h3>
+            <ArrowDown className={classes.ArrowDown}/>
+        </div>
+       
         <ul className={classes.FooterColumnList}>
           {renderListItems(footerListItems.helpSection)}
         </ul>
       </div>
       <div className={classes.FooterColumn}>
-        <h3>Follow Us</h3>
+      <div className={classes.ArrowDownColumnContainer}>
+          <h3>Follow Us</h3>
+            <ArrowDown className={classes.ArrowDown}/>
+        </div>
         <ul className={classes.FooterColumnList}>
           {renderListItems(footerListItems.followUsSection)}
         </ul>
       </div>
       <div className={classes.FooterColumn}>
-        <h3>Company</h3>
+      <div className={classes.ArrowDownColumnContainer}>
+          <h3>Company</h3>
+            <ArrowDown className={classes.ArrowDown}/>
+        </div>
         <ul className={classes.FooterColumnList}>
           {renderListItems(footerListItems.companySection)}
         </ul>
