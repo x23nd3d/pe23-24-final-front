bdf7f22a335adbc76244c0883aa1adf458352eac
MacBook <macbook@MacBook-Pro-MacBook-3.local>
2021-07-09 00:21:23 +0300

    mp_SLIDER react-alice-carousel

--- src/components/Main Page/MainPage.jsx
+++ src/components/Main Page/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PageContainer from "./Page Container/PageContainer";
-import Slider from "./Slider/Slider";
+import Slider from "./Main Page Slider/Slider";
 import Footer from "./Footer/Footer";
 import Search from "../Header/Navbar/Search/Search"
 import classes from "./MainPage.module.scss";
