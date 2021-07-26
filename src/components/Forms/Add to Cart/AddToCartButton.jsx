ced055f9e37ddd11b486a2a704722b573696d46e
pagan <paganseacrest@gmail.com>
2021-07-26 18:42:04 +0300

    fix colors bug

--- src/components/Forms/Add to Cart/AddToCartButton.jsx
+++ src/components/Forms/Add to Cart/AddToCartButton.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import AddToCartIcon from "../../UI/SVG Icons Components/AddToCartIcon";
 import isAvailableCart from "../../../utils/isAvailable";
-import "./AddToCartForm.scss";
+import classes from "./AddToCartForm.module.scss";
 
 const AddToCartButton = ({isAvailable}) => (
-  <button className="submit" type="submit">
-    <div className="AddToCartButtonContainer">
+  <button className={classes.submit} type="submit">
+    <div className={classes.AddToCartButtonContainer}>
       <span
-        className="AddToCartButtonText"
+        className={classes.AddToCartButtonText}
         style={isAvailableCart(isAvailable)}
       >Add to Cart</span>
       <AddToCartIcon available={isAvailable} />
